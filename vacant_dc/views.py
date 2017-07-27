eb3271ce07200ecc0734d473e727887d78b79b4d
johannakate <jkgriffin234@gmail.com>
2017-07-26 23:00:15 -0700

    added tileset

--- vacant_dc/views.py
+++ vacant_dc/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from mapbox import Datasets
 
 
 
 def index(request):
-    context = {}
-    return render(request, 'vacant_dc/index.html', context)
+	
+	context = {}
+	return render(request, 'vacant_dc/index.html', context)
